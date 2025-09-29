03e2207e3bdd36f0e0aa73e878bb42958bb2c618
safeer2413 <sstnadayara@gmail.com>
2025-09-30 01:01:56 +0530

    update axios file

--- src/Axios.js
+++ src/Axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const Todoinstance = axios.create({
-    baseURL: 'http://localhost:5000/api/todo',
-    timeout: 5000,
-    headers: {'X-Custom-Header': 'foobar'}
-  });
+  baseURL: import.meta.env.VITE_API_URL + "/api/todo",
+  timeout: 5000,
+  headers: { 'X-Custom-Header': 'foobar' }
+});
 
-  export default Todoinstance
+export default Todoinstance
